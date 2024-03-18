19fff19d35858fa416c1521b01fb43f4fd766042
DevJaimeGR <dev.backend.jigr@hotmail.com>
2024-03-17 19:30:49 -0500

    update jenkinsfile

--- functionslib.groovy
+++ functionslib.groovy
@@ -1,15 +1,21 @@
 // dockerlib.groovy
 
 def pushDockerImage(String DOCKER_CRED_USR, String DOCKER_CRED_PSW, String IMAGE_NAME, String VERSION, String REGISTRY) {
-    sh """
+    node{
+        sh """
         docker login --username=$DOCKER_CRED_USR --password=$DOCKER_CRED_PSW
         docker tag $IMAGE_NAME:$VERSION $REGISTRY/$IMAGE_NAME:$VERSION
         docker push $REGISTRY/$IMAGE_NAME:$VERSION
-    """
+        """
+    }
+    
 }
 
 def buildImage(String IMAGE_NAME, String VERSION){
-    sh """
-    docker build -t $IMAGE_NAME:$VERSION .
-    """ 
+    node{
+        sh """
+        docker build -t $IMAGE_NAME:$VERSION .
+        """ 
+    }
+
 }
